a01e5f7e4f0a608b2b6101c8fea35a39225ab35d
poketopa <lhs5427ll@gmail.com>
2025-09-04 15:59:21 +0900

    TASK-19 feat: PR 템플릿 작성

---
+++ backend/nft-mint-transfer/src/main/java/dev/nft_mint_transfer/NftMintTransferApplication.java
@@ -0,0 +1,13 @@
+package dev.nft_mint_transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftMintTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftMintTransferApplication.class, args);
+	}
+
+}
