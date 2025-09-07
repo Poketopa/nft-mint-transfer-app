38e5434877d47f003ffc8e1f6d377041d686f301
poketopa <lhs5427ll@gmail.com>
2025-09-08 01:18:01 +0900

    TASK-21 refactor: 로그인 / 회원가입 response DTO 수정

--- backend/nft-mint-transfer/src/main/java/dev/nft_mint_transfer/config/MultipartJackson2HttpMessageConverter.java
+++ backend/nft-mint-transfer/src/main/java/dev/nft_mint_transfer/config/MultipartJackson2HttpMessageConverter.java
@@ -1,23 +1,25 @@
 package dev.nft_mint_transfer.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Component
 public class MultipartJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
 
-    public MultipartJackson2HttpMessageConverter(ObjectMapper objectMapper) {
-        super(objectMapper, MediaType.APPLICATION_OCTET_STREAM);
-    }
+	public MultipartJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+		super(objectMapper, MediaType.APPLICATION_OCTET_STREAM);
+	}
 
-    @Override
-    public boolean canRead(Class<?> clazz, MediaType mediaType) {
-        return mediaType != null && (
-                MediaType.APPLICATION_OCTET_STREAM.equals(mediaType) ||
-                MediaType.APPLICATION_JSON.equals(mediaType) ||
-                MediaType.TEXT_PLAIN.equals(mediaType)
-        );
-    }
+	@Override
+	public boolean canRead(Class<?> clazz, MediaType mediaType) {
+		return mediaType != null && (
+			MediaType.APPLICATION_OCTET_STREAM.equals(mediaType) ||
+				MediaType.APPLICATION_JSON.equals(mediaType) ||
+				MediaType.TEXT_PLAIN.equals(mediaType)
+		);
+	}
 }
+
